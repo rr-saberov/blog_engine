a230e805cd3496c158afc346622c4085e7634287
= <=>
2021-02-05 15:56:57 +0300

    add liquibase

--- src/main/java/ru/spring/app/engine/Main.java
+++ src/main/java/ru/spring/app/engine/Main.java
@@ -1,11 +1,20 @@
 package ru.spring.app.engine;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:src/main/resources/databaseChangeLog.xml");
+        return liquibase;
     }
 }
