4092cce32fbf8bffdd1911702751868bd6588ade
= <=>
2021-02-06 19:29:52 +0300

    add db

--- src/main/java/ru/spring/app/engine/Main.java
+++ src/main/java/ru/spring/app/engine/Main.java
@@ -1,9 +1,7 @@
 package ru.spring.app.engine;
 
-import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
