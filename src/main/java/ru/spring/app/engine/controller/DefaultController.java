80da0c31b1213a3de7d608f7e3e3416288229886
= <=>
2021-02-10 22:11:00 +0300

    fix db

--- src/main/java/ru/spring/app/engine/controller/DefaultController.java
+++ src/main/java/ru/spring/app/engine/controller/DefaultController.java
@@ -3,11 +3,20 @@ package ru.spring.app.engine.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.spring.app.engine.api.response.InitResponse;
 
 @Controller
 @RequestMapping("/")
 public class DefaultController {
+
+    private final InitResponse initResponse;
+
+    public DefaultController(InitResponse initResponse) {
+        this.initResponse = initResponse;
+    }
+
     public String index(Model model) {
+        System.out.println(initResponse.getTitle());
         return "index";
     }
 }
